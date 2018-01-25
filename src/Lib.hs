a6725ef7c446d3c32247f202996241f793882136
Ibnu Daru Aji <teratower8@gmail.com>
2018-01-25 10:46:52 +0700

    added footer and blog license.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,8 @@
 module Lib
-  ( doingSomething
+  ( module Exports
   ) where
 
-import           Data.Text     (intercalate, pack)
-
-import           Lib.Generate
-import           Lib.Prelude   hiding (intercalate)
-import           Lib.ReadWrite
-import           Lib.Types
-
-doingSomething :: IO ()
-doingSomething = do
-  args <- getArgs
-  case args of
-    "new":ctype:titles ->
-      createMarkdownFile (pack ctype) (intercalate " " . map pack $ titles)
-    ["compile"] -> do
-      deleteOnlyFiles $ pathGenerated defaultConfig
-      fullFledgedHtmlGeneration defaultConfig
-    ["clean"] -> deleteOnlyFiles $ pathGenerated defaultConfig
-    _ -> do
-      putStrLn ("should be one of the following:" :: Text)
-      putStrLn ("new post title_of_post" :: Text)
-      putStrLn ("new page title_of_page" :: Text)
-      putStrLn ("compile" :: Text)
-      putStrLn ("clean" :: Text)
+import           Lib.Generate  as Exports
+import           Lib.Prelude   as Exports
+import           Lib.ReadWrite as Exports
+import           Lib.Types     as Exports
