27a0de355dbf7f9242febbd09fdd5282d9fc3cda
Ibnu D. Aji <teratower8@gmail.com>
2018-04-26 20:13:16 +0700

    lol. forgot to add favicon.

--- src/Lib/Generate.hs
+++ src/Lib/Generate.hs
@@ -50,6 +50,7 @@ skeleton Configuration {..} mymenu titleContent markdownContent =
       H.meta ! name "viewport" ! value "width=device-width, initial-scale=1.0, user-scalable=yes"
       H.meta ! name "author" ! value (textValue author)
       H.link ! rel "stylesheet" ! type_ "text/css" ! href "static/pure-min-side-menu.css"
+      H.link ! rel "icon" ! href "favicon.ico"
       H.title $ text $ siteName <> " - " <> titleContent
     body $ do
       div ! id "layout" $ do
